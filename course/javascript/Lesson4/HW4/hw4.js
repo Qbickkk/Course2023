54dc7bc8444adc68e6b9bee3ceed92e0367e8e69
Qbickkk <illia.shulika@gmail.com>
2023-05-23 01:55:38 -0700

    22.05(1) commit

--- course/javascript/Lesson4/HW4/hw4.js
+++ course/javascript/Lesson4/HW4/hw4.js
@@ -17,104 +17,118 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-// const aa = 20;
-// const bb = 50;
-//
-// function area(a, b) {
-//     let result = a * b;
-//     return result;
+
+// function area(a,b) {
+//     return a*b;
 // }
 //
-// console.log(area(aa,bb));
+// console.log(area(10,5));
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
 
-// function func(r, p) {
-//     let radius = Math.PI * (r**2);
-//     return radius;
+// function areaCircle(r) {
+//     return Math.PI * r**2;
 // }
 //
-// const radiusCircle = func(5, 3.14);
-// console.log(radiusCircle);
+// console.log(areaCircle(5));
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
 
-// function areaCilindr(R,h) {
-//     let pi = 3.14;
-//     let area = 2 * pi * R* (R + h);
-//     return area;
+// function cilindr(r,h) {
+//     return Math.round((2 * Math.PI * r * (r+h)));
 // }
 //
-// let result = areaCilindr(9,15);
-// console.log(result);
+// console.log(cilindr(4,10));
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//- створити функцію яка приймає масив та виводить кожен його елемент
-// const mas = [22,354,'gosha', false, 45,'nickelodium'];
-// function x(array) {
-//     for (const items of array) {
-//          console.log(`items : ${items}`);
+// - створити функцію яка приймає масив та виводить кожен його елемент
+
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+//
+// function xxx(array) {
+//     for (const item of array) {
+//         console.log(item);
 //     }
 // }
-// x(mas);
+//
+// xxx(users);
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
 
-// function paragraph(argument) {
-//     document.write(`<p>${argument}</p>`);
+// function paragph(argument, tag) {
+//     document.write(`<${tag}> ${argument} </${tag}>`)
 // }
-// let result = paragraph(`bebebebeb`);
-// console.log(result);
+//
+// paragph('some text', 'h2');
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
 
-// function xxx(argument) {
-//     document.write(`
-//     <ul>
-//          <li>${argument}</li>
-//          <li>${argument}</li>
-//          <li>${argument}</li>
-//     </ul>
-//     `);
+// function lizki(arg) {
+//     document.write(`<ul>`);
+//
+//     document.write(`<li>${arg}</li>`);
+//     document.write(`<li>${arg}</li>`);
+//     document.write(`<li>${arg}</li>`);
+//
+//
+//     document.write(`</ul>`);
 // }
-// xxx('viktory');
+//
+// lizki(`chinazes`);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 
-// function xxx(text, numberOfLi) {
+// function lizhki(arg, number) {
 //     document.write(`<ul>`);
-//     for (let i = 0; i < numberOfLi; i++) {
-//          document.write(`<li>${text}</li>`);
 //
+//     for (let i = 0; i < number; i++) {
+//         document.write(`<li> ${arg} </li>`);
 //     }
+//
 //     document.write(`</ul>`);
 // }
 //
-// xxx(`lolipop`, 598);
+// lizhki(`some text`, 6);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+
+// let arr = [123,41,true,'string', 34,false,'lolipop', 488];
 //
-// const mas = [23, true, 'gigi', 345, 'oven', 'bottom', 'ice', false, 28];
-// function xxx(arr) {
+// function xxx(array) {
+//
 //     document.write(`<ul>`);
-//     for (let i = 0; i < mas.length; i++) {
-//         document.write(`<li>${mas[i]}</li>`)
+//     for (const item of array) {
+//         document.write(`<li>${item}</li>`);
 //     }
 //     document.write(`</ul>`);
 // }
-// xxx(mas);
+//
+// xxx(arr);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -128,53 +142,33 @@
 //     {id: 7243, name: 'Sicasio', age: 27},
 //     {id: 4298, name: 'Duveini', age: 37}
 // ];
-// function usersPrinter(arr) {
+//
+// function sortOfmas(arr) {
 //     for (const user of arr) {
-//         document.write(`<div>${user['id']} - ${user['name']} - ${user['age']} </div>`);
+//         document.write(`<div> ${user.id} - ${user.name} - ${user.age}</div>`);
 //     }
 // }
 //
-// usersPrinter(mas);
+// sortOfmas(mas);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // - створити функцію яка повертає найменьше число з масиву
 
-// const mas = [1122,23,43,535,64646,34,6346,3656,2234,264];
-// function Num(arr) {
-//     let result = arr[0];
-//     for (let i = 1; i < mas.length; i++) {
-//         if (result > mas[i]){
-//             result = mas[i];
+// const mas = [23,4546,67,3, 239,23,999,2,1,89];
+//
+// function xxx(array) {
+//     let result = array[0];
+//     for (let i = 1; i < array.length; i++) {
+//         if (array[i] < array[0]){
+//             result = array[i];
 //         }
 //     }
 //     return result;
 // }
 //
-// const x = Num(mas)
-// console.log(x);
+// console.log(xxx(mas));
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
-
-
-// const mas = [1,2,10];
-//
-// function sum(arr) {
-//     let result = 0;
-//     for (const item of arr) {
-//         result += item;
-//     }
-//     return result;
-// }
-//
-// console.log(sum(mas));
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
-// Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
-
-function swap(arr,index1,index2) {
-
-}
