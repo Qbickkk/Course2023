66a3ce99297e75434b77faa915ebd36e586deec2
Qbickkk <illia.shulika@gmail.com>
2023-05-30 03:39:53 -0700

    30.05 commit

--- course/javascript/lesson6/hw6/hw6.js
+++ course/javascript/lesson6/hw6/hw6.js
@@ -56,3 +56,60 @@
 //     взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
 // --написати пошук всіх об'єктів, в який в modules є sass
 // --написати пошук всіх об'єктів, в який в modules є docker
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+// ['hello world', 'lorem ipsum', 'javascript is cool'].map((item) => console.log(item.length));
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+// const items = ['hello world', 'lorem ipsum', 'javascript is cool'];
+// items.map((item) => console.log(item.toUpperCase()));
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+// const items2 = ['HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'];
+// items.map((item) => console.log(item.toLowerCase()));
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+//
+// let str = ' dirty string   ';
+// let trim = str.trim();
+// console.log(trim);
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//     let str = 'Ревуть воли як ясла повні';
+// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+
+// let str = 'Ревуть воли як ясла повні';
+// let split = str.split(' ');
+// console.log(split);
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+
+// let arr = [10,8,-7,55,987,-1011,0,1050,0];
+// let map = arr.map(value => value.toString());
+// console.log(map);
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+//     let nums = [11,21,3];
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+
+let nums = [11,21,3];
+
+const sortNums = (num, direction) => {
+    switch (direction) {
+        case 'ascending':
+           return num.sort((a1,a2) => a1-a2);
+        case 'descending':
+            return num.sort((a1,a2) => a2-a1);
+    }
+}
+
+console.log(sortNums(nums, 'ascending'));
+console.log(sortNums(nums, 'descending'));
