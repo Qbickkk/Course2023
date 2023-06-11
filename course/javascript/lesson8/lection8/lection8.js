377497aa0653f6e9d0423e07ad9830f46e23859e
Qbickkk <illia.shulika@gmail.com>
2023-06-11 04:07:33 -0700

    11.06 commit

--- course/javascript/lesson8/lection8/lection8.js
+++ course/javascript/lesson8/lection8/lection8.js
@@ -185,8 +185,168 @@
 // console.log(user2p.hasOwnProperty('surname'));
 // console.log(user2p);
 ////////////////////////////////////////////////////////////////////////
+////  LECTION8
+//constructors
+
+// function User(name,age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+// let u1 = new User('kokos',123);
+// console.log(u1);
+//
+// let u2 = new User('abricos', 111);
+// console.log(u2);
+
+// let simpleUsers = [
+//     new User('asd',123),
+//     new User('asd',123),
+//     new User('asd',123),
+//     new User('asd',123),
+//     new User('asd',123),
+//     new User('asd',123),
+//     new User('asd',123),
+//     new User('asd',123)
+// ];
+// console.log(simpleUsers);
+
+//
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+//
+// function Customer(name,age,id) {
+//     this.name = name;
+//     this.age = age;
+//     this.id = id;
+// }
+//
+// let customers = users.map(value => new Customer(value.name,value.age,new Date().getTime()));
+// console.log(customers);
+//
+// //let array = [10,2,1,33,3];// [10,2,1,33,3]
+// // або те саме
+// let array = new Array(10,2,1,33,3); // [10,2,1,33,3]
+//
+// let date = new Date();
+
+// function User(name,age,wifename,wifeage) {
+//     this.name = name;
+//     this.age = age;
+//     this.wife = {
+//         wifename,
+//         wifeage
+//     };
+//
+//     this.greeting = function (msg) {
+//         console.log(`${msg} my name is ${this.name}`);
+//     };
+//
+// }
+//
+// let user = new User('vasya', 31, 'anna',29);
+// user.greeting('hello');
+//
+//
+// let friend = {
+//     name: 'kokos',
+//     skills: ['java','js']
+// }
+
+// user.greeting.call(friend, 'hi');
+// user.greeting.call(friend, ['hi']);
+//
+// user.greeting.apply(friend, ['aloha']);
+
+// let greetingCopy = user.greeting.bind(friend);
+// greetingCopy('ooooooo!');
+// greetingCopy('asdasdasd!');
+// greetingCopy('jojcorjfcoj!');
+
+//// prototype
+
+// User.prototype.foo = function () {
+//     console.log('foo works');
+// };
+//
+// User.prototype.bar = 'whisky';
+//
+// let user1 = new User('Petya', 32, 'Olya', 28);
+// console.log(user1);
+// user1.foo()
+// console.log(user1.bar);
+
+// let user = {
+//     id:123,
+//     name: 'Vasya'
+// }
+
+// let user2 = Object.create(user);
+// console.log(user);
+// console.log(user2);
+// console.log(user2.name);
+
+// console.log(user2); // id name ?
+// чи існує якийсь об'єкт з якого user2 бере прототип?
+// console.log(user2.id);
+// console.log(user2.hasOwnProperty('id'));
+//
+// console.log(user2.__proto__);
+
+////// class
 
 
+// class User {
+//     name;
+//     age;
+//     skills;
+//
+//
+//
+//     constructor(name, age, skills) {
+//         this.name = name;
+//         this.age = age;
+//         this.skills = skills;
+//     }
+// }
 
 
+// class Customer extends User {
+//
+//     constructor(name,age, id) {
+//         super(name,age);
+//         this.id = id;
+//     }
+//
+//
+// }
 
+// let customer = new Customer('ferferf', 2323,23423);
+// console.log(customer.name);
+
+// let Dog = class extends User{
+//    
+// }
+
+////////
+// function Car(model,power) {
+//     this.model = model;
+//     this.power = power;
+// }
+
+
+// function SuperCar(model,power,volume) {
+//     Car.apply(this/*supercar this*/, arguments /*arguments це [model,power,volume]*/);
+//     this.volume = volume;
+// }
