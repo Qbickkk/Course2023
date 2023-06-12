48be7f9ca4107913fdd98991652e0e445e75ba85
Qbickkk <illia.shulika@gmail.com>
2023-06-11 19:06:21 -0700

    11.06 commit

--- course/javascript/lesson8/hw8/hw8.js
+++ course/javascript/lesson8/hw8/hw8.js
@@ -39,42 +39,38 @@
 // - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
 // створити пустий масив, наповнити його 10 об'єктами Client
 
-class Client {
-
-    constructor(id,name,surname,email,phone,order) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.phone = phone;
-        this.order = order;
-    }
-
-}
-
-let clients = [
-    new Client('1','Vasya','Pupkin','pupok@gmail.com','3806878567',['tovar1', 'tovar2','tovar3']),
-    new Client('2','Petya','Petkin','pupok@gmail.com','380634878567',['tovar1', 'tovar2','tovar3']),
-    new Client('3','Jora','Jorkin','pupok@gmail.com','380687867567',['tovar1', 'tovar2','tovar3']),
-    new Client('4','Dima','Dimkin','pupok@gmail.com','380687893567',['tovar1', 'tovar2','tovar3']),
-    new Client('5','Sergey','Sergesha','pupok@gmail.com','380026878567',['tovar1', 'tovar2','tovar3']),
-    new Client('6','Illia','Pustov','pupok@gmail.com','380687298567',['tovar1', 'tovar2','tovar3']),
-    new Client('7','Bogdan','Pupkin','pupok@gmail.com','380687833567',['tovar1', 'tovar2','tovar3']),
-    new Client('8','Eduard','Pupkin','pupok@gmail.com','380687398567',['tovar1', 'tovar2','tovar3']),
-    new Client('9','Vitaliy','Pupkin','pupok@gmail.com','380687812567',['tovar1', 'tovar2','tovar3'],
-    new Client('10','Stepan','Pupkin','pupok@gmail.com','380687278567',['tovar1', 'tovar2','tovar3'])
-];
+// class Client {
+//
+//     constructor(id,name,surname,email,phone,order) {
+//         this.id = id;
+//         this.name = name;
+//         this.surname = surname;
+//         this.email = email;
+//         this.phone = phone;
+//         this.order = order;
+//     }
+//
+// }
+//
+// let clients = [
+//     new Client('1','Vasya','Pupkin','pupok@gmail.com','3806878567',['tovar1', 'tovar2','tovar3','tovar4']),
+//     new Client('2','Petya','Petkin','pupok@gmail.com','380634878567',['tovar1', 'tovar2','tovar3']),
+//     new Client('3','Jora','Jorkin','pupok@gmail.com','380687867567',['tovar1', 'tovar2','tovar3']),
+//     new Client('4','Dima','Dimkin','pupok@gmail.com','380687893567',['tovar1', 'tovar2','tovar3']),
+//     new Client('5','Sergey','Sergesha','pupok@gmail.com','380026878567',['tovar1', 'tovar2','tovar3']),
+//     new Client('6','Illia','Pustov','pupok@gmail.com','380687298567',['tovar1', 'tovar2']),
+//     new Client('7','Bogdan','Pupkin','pupok@gmail.com','380687833567',['tovar1', 'tovar3']),
+//     new Client('8','Eduard','Pupkin','pupok@gmail.com','380687398567',['tovar1', 'tovar2','tovar3']),
+//     new Client('9','Vitaliy','Pupkin','pupok@gmail.com','380687812567',['tovar1']),
+//     new Client('10','Stepan','Pupkin','pupok@gmail.com','380687278567',['tovar1', 'tovar2','tovar3'])
+// ];
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 // - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 
-
-
-
-
-
-
+// let sort = clients.sort((client1,client2) => client1.order.length - client2.order.length);
+// console.log(sort);
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -84,8 +80,18 @@ let clients = [
 // -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
-//
-//
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 // - (Те саме, тільки через клас)
 // Створити клас який дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
 // -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
